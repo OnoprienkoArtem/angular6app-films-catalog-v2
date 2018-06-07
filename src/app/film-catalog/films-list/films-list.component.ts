ec2c46917d457f1a36b31fdd4d145c7c31730456
Ilya Gil <ilya.gil@vectaury.io>
2018-06-07 23:17:14 +0300

    create card use matd

--- src/app/film-catalog/films-list/films-list.component.ts
+++ src/app/film-catalog/films-list/films-list.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FilmService } from '../film.service';
+// import { FilmService } from '../film.service';
 @Component({
   selector: 'app-films-list',
   templateUrl: './films-list.component.html',
   styleUrls: ['./films-list.component.css']
 })
 export class FilmsListComponent implements OnInit {
-  items: object[] = [];  
+  // items: object[] = [];  
 
-  constructor(public filmsService: FilmService) { }
+  constructor() { }
 
   favorites: number = 0;
 
@@ -27,4 +27,3 @@ export class FilmsListComponent implements OnInit {
     } 
   }
 
-}
