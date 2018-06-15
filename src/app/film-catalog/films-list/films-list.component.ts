48b721467dca99a3ff8693740cf79b29ff54c16b
OnoprienkoArtem <o.artem.e@gmail.com>
2018-06-15 22:59:48 +0300

    done sorting

--- src/app/film-catalog/films-list/films-list.component.ts
+++ src/app/film-catalog/films-list/films-list.component.ts
@@ -12,15 +12,36 @@ export class FilmsListComponent implements OnInit {
   favorites: number = 0;
   allFilms;
   selectedValue: string;
+
   sortOptions = [
+    { value: 'default', viewValue: 'По умолчанию' },
     { value: 'ASC', viewValue: 'По алфавиту: A-Z' },
     { value: 'DESC', viewValue: 'По алфавиту: Z-A' }
   ];
   show = true;
 
   private filteredFilms: Object[];
-  private filmsOnPage: number = 6;
-  
+  private filmsOnPage: number = 3;
+
+  doSort(order: string) {
+    (order === 'default' || order === '') ?
+      (this.filteredFilms.sort(this.compareValues('id', order)), this.selectedValue = '') :
+      this.filteredFilms.sort(this.compareValues('name', order));
+  }
+
+  compareValues(key, order = 'ASC') {
+    return (a, b) => {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) { return 0; }
+      const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+      const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+      let comparison = 0;
+      if (varA > varB) { comparison = 1; }
+      else if (varA < varB) { comparison = -1; }
+      return ((order === 'DESC') ? (comparison * -1) : comparison);
+    };
+  }
+
+ 
   constructor(public filmsService: FilmService) { }
 
   addFilmObj(filmObj) {    
@@ -28,17 +49,6 @@ export class FilmsListComponent implements OnInit {
     this.favorites = this.filmFiltered.length; 
   } 
 
-  sortList(elem, option?) {
-    if (option === 'DESC') {
-      return elem.sort(this.sortByName).reverse();
-    }
-    return elem.sort(this.sortByName);
-  }
-  sortByName(a, b) {
-    if (a.name < b.name) { return -1; }
-    if (a.name > b.name) { return 1; }
-    return 0;
-  }
 
   loadFilms(cnt: number = 3) {
     this.filmsOnPage += cnt;
